a51d8a38fe176ea87697e5bf2acc014798f093ff
spicalous <benlertlumprasertkul@gmail.com>
2023-08-30 02:08:15 +0100

    initial index page

---
+++ README.md
@@ -0,0 +1,12 @@
+# eop-website
+
+## Dependencies
+
+- Ruby 
+- Jekyll `gem install bundler jekyll`
+
+> Tip: Ensure installed gems are on your path. Use `gem environment` to find out location of installed gems
+
+## Local development
+
+Run `start.sh`
---
+++ start.sh
@@ -0,0 +1,5 @@
+# script for starting dev server
+#1. `--base-url /eop` is used so that links are correct when built with Github pages
+#2. `--config _config.yml,_config_development.yml` override any prod config with dev config (e.g. analytics)
+
+bundle exec jekyll serve --baseurl /dmc-monkslife --config _config.yml,_config_development.yml
